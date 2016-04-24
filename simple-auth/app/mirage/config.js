0ed763e4ff0143211954f6b5a569d86edfeb7374
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-04-24 13:12:39 +0200

    [chore] add project structure

---
+++ simple-auth/app/mirage/config.js
@@ -0,0 +1,25 @@
+export default function() {
+  this.get('/teammembers', function() {
+    return {
+      data: [{
+        type: 'teammembers',
+        id: 1,
+        attributes: {
+          name: 'Michael Andorfer',
+          github: 'andorfermichael',
+          email: 'mandorfer.mmt-b2014@fh-salzburg.ac.at',
+          image: 'https://avatars3.githubusercontent.com/u/11174651?v=3&s=400'
+        }
+      }, {
+        type: 'teammembers',
+        id: 2,
+        attributes: {
+          name: 'Nicola Deufemia',
+          github: 'dagopert',
+          email: 'ndeufemia.mmt-b2014@fh-salzburg.ac.at',
+          image: 'https://avatars0.githubusercontent.com/u/11182724?v=3&s=400'
+        }
+      }]
+    };
+  });
+}
