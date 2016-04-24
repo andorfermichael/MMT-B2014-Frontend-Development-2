4b534bd0be438e1e87a9d6d24fea53735e9ea12a
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2016-04-24 14:25:32 +0200

    [style] make code more readable

--- simple-auth/app/controllers/login.js
+++ simple-auth/app/controllers/login.js
@@ -5,9 +5,9 @@ export default Ember.Controller.extend({
 
   actions: {
     authenticate() {
-      this.getProperties('identification', 'password');
+      let { identification, password } = this.getProperties('identification', 'password');
 
-      this.get('session').authenticate('autenticator:oauth2', 
+      this.get('session').authenticate('authenticator:oauth2', 
       	identification, password).catch((reason) => {
       		this.set('errorMessage', reason.error);
       	}
